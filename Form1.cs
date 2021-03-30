245825d75d109549d39b8f8f862da4591f7b10da
techzin <61809691+techzin@users.noreply.github.com>
2021-03-30 14:31:12 -0300

    Add DownloadFileFromUrl()
    Add Download Function to Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CG.Web.MegaApiClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +21,22 @@ namespace Mega2021Downloader
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public void DownloadFileFromUrl(string url)
+        {
+            var client = new MegaApiClient();
+            client.LoginAnonymous();
+
+            Uri folderLink = new Uri("https://mega.nz/#F!e1ogxQ7T!ee4Q_ocD1bSLmNeg9B6kBw");
+            IEnumerable<INode> nodes = client.GetNodesFromLink(folderLink);
+            foreach (INode node in nodes.Where(x => x.Type == NodeType.File))
+            {
+                Console.WriteLine($"Downloading {node.Name}");
+                client.DownloadFile(node, node.Name);
+            }
+
+            client.Logout();
         }
     }
 }
